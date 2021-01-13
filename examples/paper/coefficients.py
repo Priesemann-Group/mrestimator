fd0b485ab8bc8b0ef6e996f2a7f480023dbe9a5c
pspitzner <paul.spitzner@ds.mpg.de>
2021-01-13 11:54:14 +0100

    fix for new api that needs method selection

--- examples/paper/coefficients.py
+++ examples/paper/coefficients.py
@@ -3,6 +3,7 @@
 # Demonstrates the subsampling bias.
 # ------------------------------------------------------------------------------ #
 
+import os
 import mrestimator as mre
 import matplotlib.pyplot as plt
 
@@ -10,9 +11,9 @@ bp_full = mre.simulate_branching(a=1000, m=0.98, numtrials=50, length=20000)
 bp_subs = mre.simulate_subsampling(bp_full, prob=0.02)
 bp_subz = mre.simulate_subsampling(bp_full, prob=0.001)
 
-rk_full = mre.coefficients(bp_full, steps=(1, 500), dt=1, dtunit="steps", description="fully sampled")
-rk_subs = mre.coefficients(bp_subs, steps=(1, 500), dt=1, dtunit="steps", description="subsampled to 2%")
-rk_subz = mre.coefficients(bp_subz, steps=(1, 500), dt=1, dtunit="steps", description="subsampled to 0.1%")
+rk_full = mre.coefficients(bp_full, steps=(1, 500), dt=1, method='ts', dtunit="steps", description="fully sampled")
+rk_subs = mre.coefficients(bp_subs, steps=(1, 500), dt=1, method='ts', dtunit="steps", description="subsampled to 2%")
+rk_subz = mre.coefficients(bp_subz, steps=(1, 500), dt=1, method='ts', dtunit="steps", description="subsampled to 0.1%")
 
 fit_full = mre.fit(rk_full)
 fit_subs = mre.fit(rk_subs)
